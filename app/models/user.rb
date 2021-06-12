d4100c664301d69f8817e5929bfa39037db577b9
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 08:19:17 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :comments
-    has_many :jobs, through: :comments
+    has_many :commented_jobs, through: :comments, source: :job   
+    #"job" above matches belongs_to :job in comments model
     has_many :jobs #that are created by user
 end
