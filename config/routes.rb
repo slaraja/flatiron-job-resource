46ff9ed63509c110313f3ef29d0b8419a2d6c335
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 12:19:10 -0400

    updated user create method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
   #don't need a post signup because resources has it covered, and users don't see the URL
   resources :comments
