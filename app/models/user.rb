5f69e3234b466ea44a20dd1186973595ef16d3d9
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 10:04:46 -0400

    added has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :commented_jobs, through: :comments, source: :job   
     #"job" above matches belongs_to :job in comments model
     has_many :jobs #that are created by user
+
+    has_secure_password
 end
