d2f1b316c667d09393894f3d7d428587ddf439ce
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 16:01:55 -0400

    sessions new view and ccontrol create meth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        #does the user exist in the system
+        @user = User.find_by(username: params[:user][:username])
+        #did we find the user and did they enter the correct password?
+        if @user.try(.authenticate, params[:user][:username])
+            
+        end
+        byebug
     end
     
     def destroy
