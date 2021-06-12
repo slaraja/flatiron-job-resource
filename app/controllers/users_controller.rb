35acc48de91995564c3b075ef66154ccb2706df5
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 09:55:01 -0400

    updated create in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
     end
 
     def show
-        
+
     end
 end
  
