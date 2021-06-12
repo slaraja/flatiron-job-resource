fc855b93bab401afb07558746af162930cfb119a
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-12 08:00:05 -0400

    updated models w has_many

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+  has_many :comments
+  has_many :users, through: :comments
   belongs_to :company
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :comments
+    has_many :jobs, through: :comments
+    has_many :jobs #that are created by user
 end
