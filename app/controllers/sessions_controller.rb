7df46209cd46a7ba2d54ee0bbedbc71dc86e0d27
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-13 18:32:10 -0400

    changed job model relationship order

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,8 @@ class SessionsController < ApplicationController
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             #set the user id in the session
-            redirect_to user_path(@user)
+            redirect_to root_path
+            # redirect_to user_path(@user)
         else
             flash[:alert] = "Sorry, please try again. Your login details were not correct."  
             redirect_to login_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
-            render :show
-            #render :new - show the form
+            render :new
         end
     end
 
