4bc1742e9e9e76b81d1f763300aad471f90a02a2
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-14 07:05:47 -0400

    updated user model validates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,6 @@ class SessionsController < ApplicationController
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             #set the user id in the session
-            redirect_to root_path
             # redirect_to user_path(@user)
         else
             flash[:alert] = "Sorry, please try again. Your login details were not correct."  
