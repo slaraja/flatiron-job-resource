f5d0f034a736d019841b807140d2340794dfed74
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-15 09:31:16 -0400

    updated job model and controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,8 @@
 class JobsController < ApplicationController
     def new
        @job = Job.new 
+       @job.build_company #flipped because it's a belongs to
+       #put it in new because we're not doing it in edit
+
     end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,5 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :company
 end
