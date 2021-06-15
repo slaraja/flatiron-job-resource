3fd15eefdb4e8da30c5c616ff84e20bb3fec728c
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-15 09:46:22 -0400

    updated job new view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,13 @@ class JobsController < ApplicationController
        @job = Job.new 
        @job.build_company #flipped because it's a belongs to
        #put it in new because we're not doing it in edit
-
     end
+
+
+    private
+
+    def user_params
+        params.require(:job).permit(:title, :level, :link, company_id, company_attributes: [:name][:location])
+    end
+
 end
