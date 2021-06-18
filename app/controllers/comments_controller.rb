9711350caca99bd03e54d2cfdc08e030607d5db9
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-18 07:33:47 -0400

    comments controller new

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,10 @@ class CommentsController < ApplicationController
     end
     
     def new
-
+        @job = Job.find_by_id(params[:id])
+        @review.build_comment 
+        #because it already knows about a review and is a belongs_to
     end
+
+
 end
