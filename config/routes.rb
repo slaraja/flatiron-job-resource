684acc0e810dc361cf5edb5ba1adc8ce4955adf7
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-18 07:16:03 -0400

    nested route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
   #don't need a post signup because resources has it covered, and users don't see the URL
+  
+ 
   resources :comments
-  resources :jobs
+  resources :jobs, only: [:show] do
+    # nested resource for jobs
+    resources :comments, only: [:new, :index]
+  end
+
   resources :companies
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
