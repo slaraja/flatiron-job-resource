22f45debbf3b173d7509970049be54564019875f
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-19 09:43:19 -0400

    comments new nested

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,16 @@ class CommentsController < ApplicationController
 
 
     def new
-        @job = Job.find_by_id(params[:job_id])
-        @comment = @job.comments.build
+        if @job = Job.find_by_id(params[:job_id])
+            @comment = @job.comments.build
+          else
+            @comment = Comment.new
+          end
     end
 
     def index
         
     end
     
-
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_125922) do
+ActiveRecord::Schema.define(version: 2021_06_19_134146) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "job_id", null: false
+    t.index ["job_id"], name: "index_comments_on_job_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_125922) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "jobs"
   add_foreign_key "comments", "users"
   add_foreign_key "jobs", "companies"
   add_foreign_key "jobs", "users"
