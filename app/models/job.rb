09b46646840db6f3ff4f47ceef05be482da19397
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-24 06:42:02 -0400

    minor updates

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,10 @@
 class Job < ApplicationRecord
-  belongs_to :company
   belongs_to :user
+  belongs_to :company
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :company
 
   # validates :link, presence: true
-
 
 end
