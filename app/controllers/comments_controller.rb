d1ac7ae68b3b9658932e5423bf9b6fa2104230d8
Stephanie La Raja <stephanielaraja@gmail.com>
2021-06-29 06:44:50 -0700

    comment create method build

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,12 +10,26 @@ class CommentsController < ApplicationController
     end
 
     def create
-      
+      redirect_if_not_logged_in 
+      @comment = Comment.new(comment_params)
+      @comment.user_id = session[:user_id] 
+      #if it reloads, we want it to re-render the information that has been added so far
+      if @comment.save
+          #retuns same value as valid
+          # session[:user_id] = @job.id
+          #don't need to store the job id in the session
+          redirect_to comment_path(@comment)
+      else           
+          render :new
+      end
     end
 
     def index
         
     end
-    
+
+  def comment_params
+      params.require(:comment).permit(:content, :job_id)
+  end
 
 end
